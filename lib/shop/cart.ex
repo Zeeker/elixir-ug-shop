f885cdf0446fba8db97a06999c1020e2b34115a6
Sascha Wolf <sascha.wolf@grandcentrix.net>
2018-06-05 19:25:13 +0200

    Total

--- lib/shop/cart.ex
+++ lib/shop/cart.ex
@@ -4,4 +4,10 @@ defmodule Shop.Cart do
   def add_product(%__MODULE__{contents: contents} = cart, %Shop.Product{} = product) do
     %__MODULE__{cart | contents: [product | contents]}
   end
+
+  def total(%__MODULE__{contents: contents}) do
+    contents
+    |> Enum.map(& &1.price)
+    |> Enum.sum()
+  end
 end
