092edcfb15443c858903e8ec6bc0868d94ea6586
Sascha Wolf <sascha.wolf@grandcentrix.net>
2018-06-05 19:25:31 +0200

    Test: Premium Cart and Customer

--- test/shop/cart_test.exs
+++ test/shop/cart_test.exs
@@ -1,7 +1,7 @@
 defmodule Shop.CartTest do
   use ExUnit.Case, async: true
 
-  alias Shop.{Cart, Product}
+  alias Shop.{Cart, Product, User}
 
   describe ".add_product/2" do
     setup :empty_cart
@@ -30,7 +30,30 @@ defmodule Shop.CartTest do
     end
   end
 
+  describe ".total/1 for a premium user cart" do
+    setup [:empty_cart, :add_premium_user]
+
+    test "with no products is still 0", %{cart: cart} do
+      assert Cart.total(cart) == 0
+    end
+
+    test "with some products has a discount of 10%", %{cart: cart} do
+      cart =
+        cart
+        |> Cart.add_product(%Product{price: 10})
+        |> Cart.add_product(%Product{price: 5})
+
+      assert Cart.total(cart) == 15 * 0.9
+    end
+  end
+
   defp empty_cart(_context) do
     [cart: %Cart{}]
+  end
+
+  defp add_premium_user(%{cart: cart}) do
+    user = %User{premium: true}
+
+    [cart: %Cart{cart | user: user}]
   end
 end
